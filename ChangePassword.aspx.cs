b1cc32acbc6c612ff3606dbf618e196e3c14debf
pratham159 <pratham.nichani127023@marwadiuniversity.ac.in>
2026-07-08 13:21:30 +0530

    Add files via upload

---
+++ ChangePassword.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UNIT1
+{
+    public partial class ChangePassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnChange_Click(object sender, EventArgs e)
+        {
+            if (txtNewPass.Text == txtConfirmPass.Text)
+            {
+                lblMsg.Text = "Password Changed Successfully";
+            }
+            else
+            {
+                lblMsg.Text = "New Password and Confirm Password do not match";
+            }
+        }
+    }
+}
