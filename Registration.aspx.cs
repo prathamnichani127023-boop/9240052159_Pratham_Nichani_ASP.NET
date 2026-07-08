5e5314dabafaeb1a470b3be7c8ff427686417a31
pratham159 <pratham.nichani127023@marwadiuniversity.ac.in>
2026-07-08 13:22:12 +0530

    Add files via upload

---
+++ Registration.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UNIT1
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            lblMsg.Text = "Registration Successful";
+        }
+    }
+}
