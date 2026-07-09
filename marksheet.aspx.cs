ec9b9f10a1f15f3065c43af7f8a4c578365db1b4
pratham159 <pratham.nichani127023@marwadiuniversity.ac.in>
2026-07-09 20:57:46 +0530

    Add files via upload

---
+++ marksheet.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.UI;
+
+namespace UNIT2
+{
+    public partial class marksheet : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("Result.aspx");
+        }
+    }
+}
