f04eb3483f2d369928faa626529cd489302a0803
pratham159 <pratham.nichani127023@marwadiuniversity.ac.in>
2026-07-09 20:52:31 +0530

    Add files via upload

---
+++ StudentAdmission.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.UI;
+
+namespace UNIT2
+{
+    public partial class StudentAdmission : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                lblMessage.Text = "Registration Successful!";
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+            }
+        }
+    }
+}
