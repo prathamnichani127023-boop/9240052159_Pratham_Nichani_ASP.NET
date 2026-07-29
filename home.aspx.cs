6252326f30ce8809fa95670c784c9ebd724f05ed
pratham159 <pratham.nichani127023@marwadiuniversity.ac.in>
2026-07-29 20:43:09 +0530

    Add files via upload

---
+++ home.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Unit_3
+{
+    public partial class home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["name"]!=null)
+            {
+                Response.Write();
+            }
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
