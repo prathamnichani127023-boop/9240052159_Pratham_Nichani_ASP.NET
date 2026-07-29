a608d06a2d5b1562134424b957d35f3bcdc19da8
pratham159 <pratham.nichani127023@marwadiuniversity.ac.in>
2026-07-29 20:42:42 +0530

    Add files via upload

---
+++ Admission.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Unit_3
+{
+    public partial class Admission : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            Response.Write("<script>alert('Admission Form Submitted Successfully!');</script>");
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            txtName.Text = "";
+            txtEmail.Text = "";
+            txtMobile.Text = "";
+            txtAddress.Text = "";
+            ddlCourse.SelectedIndex = 0;
+            rbMale.Checked = false;
+            rbFemale.Checked = false;
+        }
+    }
+}
